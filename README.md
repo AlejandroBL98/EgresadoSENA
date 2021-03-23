18dfae2551b33d6b2f8aa4f66f2829daf0987636
AlejandroBL98 <jabetancur8146@misena.edu.co>
2021-03-23 12:39:02 -0500

    Agregar archivos de proyecto.

---
+++ README.md
@@ -0,0 +1,14 @@
+# Sistema de información egresados SENA
+
+Esta es una aplicación para egresados del SENA donde incorporamos varias metodologías de desarrollo como y lo son: Plantillas de vistas y validación de datos de los formulario.
+
+## Metodologías utilizadas
+
+- Asp.net
+- MVC
+- Sql Server
+
+## Vista del proyecto
+
+![Home!](Images/Inicio.png)
+![Details!](Images/Registrados.png)
---
+++ Siena/Controllers/HomeController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Siena.Models;
+
+namespace Siena.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Home/Details/5
+        public ActionResult Details()
+        {
+            RegistroUsuario us = new RegistroUsuario();
+            return View(us.RecupearTodos());
+            
+        }
+
+        // GET: Home/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Home/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            RegistroUsuario us = new RegistroUsuario();
+            Usuario usr = new Usuario
+            {
+                Documento = int.Parse(collection["documento"]),
+                TipoDocumento = collection["tipodocumento"],
+                Nombre = collection["nombre"],
+                Celular = int.Parse(collection["celular"]),
+                Email = collection["email"],
+                Genero = collection["genero"],
+                AreaFormacion = collection["areaFormacion"],
+                FechaEgresado = DateTime.Parse(collection["fechaegresado"].ToString()),
+                Direccion = collection["direccion"],
+                Barrio = collection["barrio"],
+                Ciudad = collection["ciudad"],
+                Departamento = collection["departamento"],
+                FechaIngreso = DateTime.Parse(collection["fechaingreso"].ToString())
+
+            };
+            us.Insertar(usr);
+
+            return RedirectToAction("Details");
+
+        }
+
+        // GET: Home/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Home/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Home/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
